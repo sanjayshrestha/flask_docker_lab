3cc8f9138e6cc8d627a2fd222563632ecec68020
Sanjay <shrestha.sanjay@outlook.com>
2018-02-12 05:03:21 +0000

    Test

--- app/config.py
+++ app/config.py
@@ -2,7 +2,7 @@
 import os
 
 # Title of the site
-SITE_TITLE = 'Sanjay Shrestha Docker Lab'
+SITE_TITLE = 'Sanjay Shresthas Docker Lab'
 # The URL for static files/images
 STATIC_URL = 'https://s3-us-west-2.amazonaws.com/flaskdockerlab-static/'
 # The name of the
